b85fa1a74b9e0de024f0a4e7d78d3cb5a0b9d472
KHWang <khwang@csie.fju.edu.tw>
2020-03-13 15:14:25 +0800

    Friday afternoon try commit and push again!

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 		System.out.println("Git is cool now and fun everyday!");
 		System.out.println("Today is Friday and it is a raining day!");
 		System.out.println("Trying EGit pull from the remote repository!");
+		System.out.println("Friday afternoon Setup Philip 276e8 monitoer!");
 	}
 
 }
