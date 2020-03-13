99ef041404605e5b2ece39ac371995107beda9f9
khwang1964 <57040197+khwang1964@users.noreply.github.com>
2020-03-13 11:26:33 +0800

    Update GitTest.java
    Try EGit fetch from the remote repository.

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -3,9 +3,10 @@ package com.vogella.git.first;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO 自動產生的方法 Stub
+		// TODO Stub
 		System.out.println("Git is cool now and fun everyday!");
 		System.out.println("Today is Friday and it is a raining day!");
+		System.out.println("Trying EGit pull from the remote repository!");
 	}
 
 }
