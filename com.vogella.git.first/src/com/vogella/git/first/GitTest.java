a64a915f55b28bc65872a1b4bd373595a3a84657
KHWang <khwang@csie.fju.edu.tw>
2020-03-13 10:10:08 +0800

    Friday modify the output message.

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
 		System.out.println("Git is cool now and fun everyday!");
+		System.out.println("Today is Friday and it is a raining day!");
 	}
 
 }
